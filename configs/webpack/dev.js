cf86a9b8981bdb72be0f26c078e3df7c64158266
정경환 <sujkh@nomadconnection.com>
2018-06-25 14:32:38 +0900

    delete webpack lifecycle

--- configs/webpack/dev.js
+++ configs/webpack/dev.js
@@ -2,21 +2,19 @@
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 const commonConfig = require('./common');
-const LifecyclePlugin = require('webpack-lifecycle-plugin')
 const openBrowser = require('react-dev-utils/openBrowser');
 const paths = require('./paths');
-let isFirstBrowserOpen = true;
 
 process.env.NODE_ENV = 'development';
 process.env.BABEL_ENV = 'development';
-
+openBrowser('http://localhost:8080');
 module.exports = merge(commonConfig, {
   entry: [
     // require.resolve('react-dev-utils/webpackHotDevClient'),
     // 'react-hot-loader/patch', // activate HMR for React
-    'webpack-dev-server/client?http://localhost:8080',// bundle the client for webpack-dev-server and connect to the provided endpoint
+    'webpack-dev-server/client?http://localhost:8080', // bundle the client for webpack-dev-server and connect to the provided endpoint
     'webpack/hot/only-dev-server', // bundle the client for hot reloading, only- means to only hot reload for successful updates
-    paths.appIndexJs,// the entry point of our app
+    paths.appIndexJs, // the entry point of our app
     paths.appStyleCss
   ],
   devServer: {
@@ -25,12 +23,6 @@ module.exports = merge(commonConfig, {
   devtool: 'cheap-module-eval-source-map',
   plugins: [
     new webpack.HotModuleReplacementPlugin(), // enable HMR globally
-    new webpack.NamedModulesPlugin(), // prints more readable module names in the browser console on HMR updates
-    new LifecyclePlugin({"done": (compilation, options, pluginOptions) => {
-      if(isFirstBrowserOpen){
-        isFirstBrowserOpen = false
-        openBrowser('http://localhost:8080');
-      }
-    }}), // webpack lifecyclePlugin
-  ],
+    new webpack.NamedModulesPlugin() // prints more readable module names in the browser console on HMR updates
+  ]
 });
